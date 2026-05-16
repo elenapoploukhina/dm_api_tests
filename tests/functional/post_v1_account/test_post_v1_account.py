5b6fc7b3087ec4314874b2ba09a668d45d6435c2
Elena Poploukhina <lenafalileeva@gmail.com>
2026-05-16 12:10:18 +0300

    Фикс имени пользователя для запуска тестов.

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -11,7 +11,7 @@ def test_post_v1_account():
     account_api = AccountApi(host="http://185.185.143.231:5051")
     login_api = LoginApi(host="http://185.185.143.231:5051")
     mailhog_api = MailhogApi(host="http://185.185.143.231:5025")
-    login = 'lenaivanova_8'
+    login = 'lenaivanova_10'
     email = f'{login}@mail.ru'
     password = '123456789'
 
