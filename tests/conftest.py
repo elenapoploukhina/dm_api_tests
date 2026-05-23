5d5986dde72ccaee84a15e6cbee00b30b55c603e
Elena Poploukhina <lenafalileeva@gmail.com>
2026-05-23 12:36:01 +0300

    Все тесты переведены на использование фикстур.

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,8 @@ structlog.configure(
     processors=[
         structlog.processors.JSONRenderer(
             indent=4,
-            ensure_ascii=True,
-            sort_keys=True
+            ensure_ascii=True
+            # sort_keys=True
         )
     ]
 )
