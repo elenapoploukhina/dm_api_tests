d22e7ac4b8dfa85b1fa4669e89ccd5cdc5b55df6
Elena Poploukhina <lenafalileeva@gmail.com>
2026-05-29 17:20:29 +0300

    Тест на логин пользователя переведен на использование Pydantic-моделей.

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -1,9 +1,13 @@
-def test_post_v1_account_login(account_helper, prepare_user):
+def test_post_v1_account_login(
+        account_helper,
+        prepare_user
+):
     login = prepare_user.login
     email = prepare_user.email
     password = prepare_user.password
 
     account_helper.register_new_user(login=login, password=password, email=email)
-    response = account_helper.user_login(login=login, password=password)
-    assert response.status_code == 200, "Пользователь не смог авторизоваться."
-    assert response.headers.get("x-dm-auth-token"), "Токен для пользователя не был получен"
+    account_helper.user_login(login=login, password=password, validate_response=True)
+    # Тут пока непонятно, как проверить одновременно и модель ответа, и статус код, и хедер
+    # assert response.status_code == 200, "Пользователь не смог авторизоваться."
+    # assert response.headers.get("x-dm-auth-token"), "Токен для пользователя не был получен"
