53055678e164e4642c8f531612ea1508e284ab9e
Elena Poploukhina <lenafalileeva@gmail.com>
2026-05-29 17:24:32 +0300

    Тест на активацию пользователя после регистрации переведен на использование Pydantic-моделей.

--- tests/functional/put_v1_acccount_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_acccount_token/test_put_v1_account_token.py
@@ -1,8 +1,11 @@
-def test_put_v1_account_token(account_helper, prepare_user):
+def test_put_v1_account_token(
+        account_helper,
+        prepare_user
+        ):
     login = prepare_user.login
     email = prepare_user.email
     password = prepare_user.password
 
     account_helper.register_new_user(login=login, password=password, email=email)
-    response = account_helper.user_login(login=login, password=password)
+    response = account_helper.user_login(login=login, password=password, validate_response=False)
     assert response.status_code == 200, "Пользователь не смог авторизоваться."
