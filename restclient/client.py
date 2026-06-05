1373ded5be7eefc5d3d61dbd5b87978941464274
Elena Poploukhina <lenafalileeva@gmail.com>
2026-06-05 22:22:27 +0300

    Добавили coverage

--- restclient/client.py
+++ restclient/client.py
@@ -4,9 +4,13 @@ from json import JSONDecodeError
 import curlify
 import structlog
 from requests import session
+from swagger_coverage_py.listener import CoverageListener
+from swagger_coverage_py.request_schema_handler import RequestSchemaHandler
 
 from restclient.configuration import Configuration
 from restclient.utilities import allure_attach
+
+from swagger_coverage_py.uri import URI
 
 
 class RestClient:
@@ -83,6 +87,11 @@ class RestClient:
         rest_response = self.session.request(method=method, url=full_url, **kwargs)
 
         curl = curlify.to_curl(rest_response.request)
+        uri = URI(host=self.host, base_path="", unformatted_path=path, uri_params=kwargs.get('params'), )
+        RequestSchemaHandler(
+            uri, method.lower(), rest_response, kwargs
+        ).write_schema()
+
         print(curl)
 
         log.msg(
